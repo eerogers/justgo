ddf7efa84d600c5abcb0fa760fa774396661c537
hurricaneronron <vprzybyl@gmail.com>
2018-05-12 20:38:59 -0700

    fixed chart function

--- public/js/userprofile.js
+++ public/js/userprofile.js
@@ -1,13 +1,16 @@
 var dps = []
-var userid = localStorage.getItem("userId")
+var userid = parseInt(localStorage.getItem("userId"))
 console.log(userid)
-$.get('/api/times/'+ userid, function(data) {
+
+$.get('/api/times/'+userid, function(data) {
 	console.log(data)
 //	console.log(moment(data[21].end_time, 'HH:mm:ss').diff(moment(data[21].start_time, 'HH:mm:ss'), 'seconds'))
 //	console.log(data[12].start_time)
 //	console.log(moment.duration(data[12].end_time.diff(data[12].start_time)).format("m[m] s[s]"))
 	for(var i = 0; i < data.length; i++) {
-		dps.push({'x': new Date(data[i].createdAt), 'y': data[i].distance})
+		dps.push({'x': new Date(data[i].date), 'y': parseInt(data[i].distance)})
+		console.log(data[i].createdAt)
+		console.log(data[i].distance)
 		$('#course').append("<div class='height'>"+ data[i].route_id +"</div>")
 		$('#distance').append("<div class='height'>"+ data[i].distance+" mi</div>")
 		$('#time').append("<div class='height'>"+ Math.round((moment(data[i].end_time, 'HH:mm:ss').diff(moment(data[i].start_time, 'HH:mm:ss'), 'minutes', 'seconds') +0.00001) * 100) / 100 +" min</div>")
